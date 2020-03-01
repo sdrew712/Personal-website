fafa1d1532b87ad1d2b29f1858bf20202f6c9955
unknown <drew.sarah.e@gmail.com>
2020-03-01 15:50:29 -0500

    added useEffect and useState to ShortStory

--- src/containers/ShortStory.js
+++ src/containers/ShortStory.js
@@ -1,9 +1,26 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
+import ShortStoryData from '../data/ShortStories.json'
 
-const ShortStory = () => {
+const ShortStory = (props) => {
+    const [story, setStory] = useState(undefined)
+
+    useEffect(() => {
+        const _story = ShortStoryData.find((data) => {
+            return data.id === props.match.params.id
+        })
+        if (_story === undefined) {
+            props.history.push("/shortstories")
+        }
+        else{
+            setStory(_story)
+        }
+    }, [props.match.params.id, props.history])
+
+    if (story === undefined) return null
     return (
         <div>
-            Fireflight page bro!!!
+            {story.title}
+            <img src={story.image} alt={story.title}/>
         </div>
     )
 }
